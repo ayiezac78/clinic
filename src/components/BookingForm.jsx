8ef75c3c241b50ae224d62f3aa3a319cec4cbce0
Ariel Maniago <admaniago01@gmail.com>
2023-04-22 08:02:16 +0800

    done with the booking form

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -34,6 +34,7 @@ const generateIncrementalId = async () => {
 
 
 function AppointmentForm() {
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -44,6 +45,7 @@ function AppointmentForm() {
 
 
   const onSubmit = async (data) => {
+    setIsLoading(true);
     const appointment_id = await generateIncrementalId();
     axios
       .post('http://localhost:8000/api/appointments/store', { ...data, appointment_id })
@@ -66,8 +68,8 @@ function AppointmentForm() {
                   <h1 style="font-size: 28px; margin-bottom: 10px; color: #333;">AWR Clinic</h1>
                   <h2 style="font-size: 24px; margin-bottom: 20px; color: #333;">Appointment Confirmation</h2>
                   <p style="font-size: 16px; margin-bottom: 20px; color: #333;">Dear ${data.first_name} ${data.last_name},</p>
-                  <p style="font-size: 16px; margin-bottom: 20px; color: #333;">Your appointment has been confirmed.</p>
-                  <p style="font-size: 16px; margin-bottom: 20px; color: #333;">These are the details of your appointment schedule:</p>
+                  <p style="font-size: 16px; margin-bottom: 20px; color: #333;">Your appointment has been confirmed!</p>
+                  <p style="font-size: 16px; margin-bottom: 20px; color: #333;">These are the details of your appointment confirmed schedule:</p>
                   <table style="border-collapse: collapse; width: 100%;">
                     <tbody>
                       <tr>
@@ -88,11 +90,12 @@ function AppointmentForm() {
                       </tr>
                     </tbody>
                   </table>
+                  <p style="font-size: 16px; margin-bottom: 20px; color: #333;">Please present it to the receptionist in order to assist you.</p>
                   <p style="font-size: 16px; margin-top: 20px; color: #333;">Thank you for choosing our AWR Clinic.</p>
                   <p style="font-size: 16px; margin-bottom: 20px; color: #333;">If you have any questions or need to reschedule, please contact us at:</p>
-                  <p style="font-size: 16px; margin-bottom: 20px; color: #333;">Phone: 123-456-7890<br />Email:contactusclinic@gmail.com</p>
+                  <p style="font-size: 16px; margin-bottom: 20px; color: #333;">Phone: 09659051766<br />Email:contactusclinic@gmail.com</p>
                   <div style="text-align: center;">
-                  <a href="https://example.com" style="display: inline-block; background-color: #333; color: #fff; padding: 10px 20px; border-radius: 4px; text-decoration: none; font-size: 16px; margin-top: 20px;">Visit our website</a>
+                  <a href="https://clinicmp2.netlify.app/" style="display: inline-block; background-color: #333; color: #fff; padding: 10px 20px; border-radius: 4px; text-decoration: none; font-size: 16px; margin-top: 20px;">Visit our website</a>
                   </div>
                   </div>
                   </div>
@@ -101,189 +104,200 @@ function AppointmentForm() {
           `,
           }).then(
             message => {
-              toast.success("Please check your email that was sent to you!");
+              toast.success("Appointment has been scheduled successfully! Please check your email");
               reset();
+              setIsLoading(false);
             }
           );
       })
       .catch((error) => {
         console.log(error);
+        setIsLoading(false);
       });
   };
 
 
   return (
     <section className="px-4 md:px-0 md:py-20 mt-[85px] bg-[#EDFDF2] font-sora grid grid-cols-1 md:grid-cols-2 gap-4">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="grid gap-4"
-      >
-        <div className="mb-4">
-          <label htmlFor="appointment_date" className="block font-medium mb-1">
-            Scheduled Date
-          </label>
-          <input
-            type="date"
-            id="appointment_date"
-            {...register("appointment_date", { required: true })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-          />
-          {errors.appointment_date && (
-            <span className="text-red-500">This field is required</span>
-          )}
-        </div>
-        <div className="mb-4">
-          <label htmlFor="schedule_time" className="block font-medium mb-1">
-            Scheduled Time
-          </label>
-          <select
-            id="schedule_time"
-            {...register("schedule_time", { required: true })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-          >
-            <option value="AM">AM</option>
-            <option value="PM">PM</option>
-          </select>
-          {errors.schedule_time && (
-            <span className="text-red-500">This field is required</span>
-          )}
-        </div>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4">
+          <h2 className="text-center font-bold text-3xl md:text-4xl mb-4 col-span-2">
+            Schedule an Appointment
+          </h2>
+          <div className="mb-4">
+            <label
+              htmlFor="appointment_date"
+              className="block font-medium mb-1"
+            >
+              Select an Appointment Date
+            </label>
+            <input
+              type="date"
+              id="appointment_date"
+              {...register("appointment_date", { required: true })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            />
+            {errors.appointment_date && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="schedule_time" className="block font-medium mb-1">
+              Select an Appointment Time
+            </label>
+            <select
+              id="schedule_time"
+              {...register("schedule_time", { required: true })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            >
+              <option value="AM">AM</option>
+              <option value="PM">PM</option>
+            </select>
+            {errors.schedule_time && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="first_name" className="block font-medium mb-1">
-            First Name
-          </label>
-          <input
-            type="text"
-            id="first_name"
-            {...register("first_name", { required: true })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-          />
-          {errors.first_name && (
-            <span className="text-red-500">This field is required</span>
-          )}
-        </div>
+          <div className="mb-4">
+            <label htmlFor="first_name" className="block font-medium mb-1">
+              First Name
+            </label>
+            <input
+              type="text"
+              id="first_name"
+              {...register("first_name", { required: true })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            />
+            {errors.first_name && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="last_name" className="block font-medium mb-1">
-            Last Name
-          </label>
-          <input
-            type="text"
-            id="last_name"
-            {...register("last_name", { required: true })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-          />
-          {errors.last_name && (
-            <span className="text-red-500">This field is required</span>
-          )}
-        </div>
-        <div className="mb-4">
-          <label htmlFor="age" className="block font-medium mb-1">
-            Age
-          </label>
-          <input
-            type="number"
-            id="age"
-            {...register("age", { required: true })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-          />
-          {errors.age && (
-            <span className="text-red-500">This field is required</span>
-          )}
-        </div>
+          <div className="mb-4">
+            <label htmlFor="last_name" className="block font-medium mb-1">
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="last_name"
+              {...register("last_name", { required: true })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            />
+            {errors.last_name && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="age" className="block font-medium mb-1">
+              Age
+            </label>
+            <input
+              type="number"
+              id="age"
+              {...register("age", { required: true })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            />
+            {errors.age && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
 
-        <div className="mb-4">
-          <label htmlFor="gender" className="block font-medium mb-1">
-            Gender
-          </label>
-          <select
-            id="gender"
-            {...register("gender", { required: true })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-          >
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="contact_number" className="block font-medium mb-1">
-            Contact Number
-          </label>
-          <input
-            type="tel"
-            id="contact_number"
-            {...register("contact_number", {
-              required: true,
-              pattern: /^\d{11}$/,
-            })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-          />
-          {errors.contact_number && (
-            <span className="text-red-500">
-              Please enter a valid 11-digit phone number
-            </span>
-          )}
-        </div>
-        <div className="mb-1">
-          <label htmlFor="email_address" className="block font-medium mb-1">
-            Email Address
-          </label>
-          <input
-            type="email"
-            id="email_address"
-            {...register("email_address", {
-              required: true,
-              pattern: /^\S+@\S+\.\S+$/,
-            })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-          />
-          {errors.email_address && (
-            <span className="text-red-500">
-              Please enter a valid email address
-            </span>
-          )}
-        </div>
+          <div className="mb-4">
+            <label htmlFor="gender" className="block font-medium mb-1">
+              Gender
+            </label>
+            <select
+              id="gender"
+              {...register("gender", { required: true })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            >
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="contact_number" className="block font-medium mb-1">
+              Contact Number
+            </label>
+            <input
+              type="tel"
+              id="contact_number"
+              {...register("contact_number", {
+                required: true,
+                pattern: /^\d{11}$/,
+              })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            />
+            {errors.contact_number && (
+              <span className="text-red-500">
+                Please enter a valid 11-digit phone number
+              </span>
+            )}
+          </div>
+          <div className="mb-1">
+            <label htmlFor="email_address" className="block font-medium mb-1">
+              Email Address
+            </label>
+            <input
+              type="email"
+              id="email_address"
+              {...register("email_address", {
+                required: true,
+                pattern: /^\S+@\S+\.\S+$/,
+              })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            />
+            {errors.email_address && (
+              <span className="text-red-500">
+                Please enter a valid email address
+              </span>
+            )}
+          </div>
 
-        <div className="mb-4 col-span-2">
-          <label htmlFor="address" className="block font-medium mb-1">
-            Address
-          </label>
-          <input
-            id="address"
-            type='text'
-            {...register("address", { required: true })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
-          ></input>
-          {errors.address && (
-            <span className="text-red-500">This field is required</span>
-          )}
-        </div>
-        <div className="md:col-span-2 lg:col-span-2 xl:col-span-2">
-          <label htmlFor="medical_concern" className="block font-medium mb-1">
-            Reason for Appointment
-          </label>
-          <textarea
-            id="medical_concern"
-            {...register("medical_concern", { required: true })}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none h-32 resize-none"
-          ></textarea>
-          {errors.medical_concern && (
-            <span className="text-red-500">This field is required</span>
-          )}
-        </div>
+          <div className="mb-4 col-span-2">
+            <label htmlFor="address" className="block font-medium mb-1">
+              Address
+            </label>
+            <input
+              id="address"
+              type="text"
+              {...register("address", { required: true })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            ></input>
+            {errors.address && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+          <div className="md:col-span-2 lg:col-span-2 xl:col-span-2">
+            <label htmlFor="medical_concern" className="block font-medium mb-1">
+              Reason for Appointment
+            </label>
+            <textarea
+              id="medical_concern"
+              {...register("medical_concern", { required: true })}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none h-32 resize-none"
+            ></textarea>
+            {errors.medical_concern && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
 
-        <div className="col-span-2">
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 w-full"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    <ToastContainer limit={1}/>
+          <div className="col-span-2">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 w-full"
+            >
+              {isLoading ? 'Loading...' : 'Submit'}
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className='grid place-items-center px-6'>
+        <p>Note:  Please check your spam folder if you don't receive an email after filling up the form. If you don't receive an email, please email us at <a href="mailto:contactusclinic@gmail.com" className='text-blue-500 underline'>contactusclinic@gmail.com</a>. Thank you!
+        </p>
+      </div>
+      <ToastContainer limit={1} />
     </section>
   );
 }
